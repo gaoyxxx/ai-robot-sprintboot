dbb45ebdc157ad0135dd0623251abcb32c81cebc
gaoyxxx <gao.yx.apply@outlook.com>
2025-06-08 21:33:53 +0800

    创建后端项目

---
+++ src/main/java/com/gaoyx/ai/robot/AiRobotSprintbootApplication.java
@@ -0,0 +1,13 @@
+package com.gaoyx.ai.robot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiRobotSprintbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiRobotSprintbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gaoyx/ai/robot/AiRobotSprintbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaoyx.ai.robot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiRobotSprintbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
